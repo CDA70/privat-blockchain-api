9c8b7cdc2db6f6944eb5066b873d6236f6b0b59c
chris70 <daemschris@gmail.com>
2018-09-23 16:42:34 +0200

    some cleanup

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express()
 const port = 8000
 const parser = require('body-parser')
 const Blockchain = require('./simpleChain')
-let blockchain = new Blockchain()
+const blockchain = new Blockchain()
+const Block = require('./block')
+const block = new Block()
+
 
 app.use(parser.json())
 app.get('/', (req, res) => res.send('Hello World!'))
@@ -25,8 +28,9 @@ app.get('/block/:blockHeight', async(req,res) => {
 
 })
 
+
 app.post('/block', async (req, res) => {
-    //const { body } = req.body;
+   // const { body } = req.body;
 
     if (req.body.body === undefined || req.body.body === '') {
         res.status(400).json({
@@ -34,14 +38,26 @@ app.post('/block', async (req, res) => {
             "message": 'Bock data cannot be empty, please fill out block!'})
 
     } else {
-       blockchain.addBlock(req.body.body)
+       await blockchain.addBlock(new Block(req.body.body)) //req.body.body)
          .then(block => res.status(201).json(req.body.body))
          .catch ((error) => {
-             res.status(500).json('not sure what the do with it yet')
+             res.status(500).json({
+                 "status": 500,
+                 "message": 'not sure what the do with it yet'})
          })
     }
+    
 })
 
-
+/*
+app.post('/block', async (req, res) => {
+    let newBlock;
+    newBlock = await blockchain.addBlock(req.body.body)
+      .catch(error => {
+      });
+    res.json(newBlock);
+  
+  });
+*/
 //curl http://localhost:8000/block/0
 //curl -X "POST" "http://localhost:8000/block" -H 'Content-Type: application/json' -d $'{"body":"block body contents"}'
